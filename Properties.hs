4e79ff5785ad54e40d537940187b412faac51a46
Ben Gamari <ben@smart-cactus.org>
2017-10-07 10:11:42 -0400

    Properties Show counterexample

--- Properties.hs
+++ Properties.hs
@@ -4,13 +4,13 @@ import Test.QuickCheck.Monadic
 import qualified Data.RingBuffer as R
 import qualified Data.Vector as V
 
-testAppend :: (Eq a)
+testAppend :: (Eq a, Show a)
            => Positive Int -> [a] -> Property
 testAppend (Positive cap) xs = monadicIO $ do
     r <- liftIO $ R.new cap :: PropertyM IO (R.RingBuffer V.Vector a)
     liftIO $ mapM_ (`R.append` r) (reverse xs)
     xs' <- liftIO $ R.toList r
-    return $ xs' == take cap xs
+    return $ counterexample (show xs') $ xs' == take cap xs
 
 main :: IO ()
 main = quickCheck (testAppend :: Positive Int -> [Int] -> Property)
